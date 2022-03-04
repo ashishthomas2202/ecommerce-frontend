08ee95419a72cc93dd96728e9c00500297ff2446
Ashish Thomas <ashishthomas2202@gmail.com>
2022-03-04 14:57:10 -0500

    footer menu added

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,5 +1,86 @@
 import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import style from './Footer.module.scss';
+import { Brand, Shop, Social } from '../../utils/settings';
 
 export default function Footer() {
-  return <div>Footer</div>;
+  const menuLists = [
+    {
+      title: `About ${Brand.name}`,
+      items: [
+        { name: 'About Us', link: '/' },
+        { name: 'Connect Socially', link: '/' },
+      ],
+    },
+    {
+      title: 'Shop',
+      items: Shop.collections,
+    },
+    {
+      title: 'Need Help?',
+      items: [
+        { name: 'Contact Us', link: '/' },
+        { name: 'Shipping Services', link: '/' },
+        { name: 'Payment Options', link: '/' },
+        { name: 'Returns & Exchanges', link: '/' },
+        { name: 'Product Care', link: '/' },
+        { name: 'FAQs', link: '/' },
+        { name: 'Unsubscrbe', link: '/' },
+        { name: 'Sitemap', link: '/' },
+      ],
+    },
+    {
+      title: 'Find Us On',
+      items: Social.list,
+    },
+  ];
+  const menu = (
+    <div className={style.menu}>
+      {menuLists.map((list, i) => {
+        return (
+          <div key={'footerList' + i}>
+            <h4>{list.title}</h4>
+            <ul>
+              {list.items.map((item, itemId) => {
+                return (
+                  <li key={item.name + itemId}>
+                    <Link href={item.link}>
+                      <a>
+                        {console.log(item.icon)}
+                        {item.icon ? (
+                          <img
+                            className={style.icon}
+                            src={item.icon}
+                            alt={item.name}
+                          />
+                        ) : (
+                          ''
+                        )}
+                        {item.name}
+                      </a>
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        );
+      })}
+    </div>
+  );
+
+  const copyright = (
+    <div className={style.copyright}>
+      ©{new Date().getFullYear()} {Brand.fullName}. All rights reserved.
+    </div>
+  );
+  return (
+    <div className={style.footer}>
+      <div className={style.container}>
+        {menu}
+        {copyright}
+      </div>
+    </div>
+  );
 }
---
+++ utils/settings.js
@@ -0,0 +1,46 @@
+export const Brand = {
+  name: 'Ecommerce',
+  fullName: 'Ecommerce LLC',
+  logo: '',
+};
+
+export const Shop = {
+  collections: [
+    { name: 'Mens', link: '/' },
+    { name: 'Womens', link: '/' },
+    { name: 'KidsWear', link: '/' },
+    { name: 'Accessories', link: '/' },
+    { name: 'Shirt', link: '/' },
+    { name: 'Jeans', link: '/' },
+  ],
+};
+
+export const Social = {
+  list: [
+    {
+      name: 'Facebook',
+      link: '/',
+      icon: 'https://e7.pngegg.com/pngimages/340/745/png-clipart-computer-icons-white-instagram-icon-text-logo.png',
+    },
+    {
+      name: 'Instagram',
+      link: '/',
+      icon: 'https://e7.pngegg.com/pngimages/340/745/png-clipart-computer-icons-white-instagram-icon-text-logo.png',
+    },
+    {
+      name: 'Twitter',
+      link: '/',
+      icon: 'https://e7.pngegg.com/pngimages/340/745/png-clipart-computer-icons-white-instagram-icon-text-logo.png',
+    },
+    {
+      name: 'Pinterest',
+      link: '/',
+      icon: 'https://e7.pngegg.com/pngimages/340/745/png-clipart-computer-icons-white-instagram-icon-text-logo.png',
+    },
+    {
+      name: 'Youtube',
+      link: '/',
+      icon: 'https://e7.pngegg.com/pngimages/340/745/png-clipart-computer-icons-white-instagram-icon-text-logo.png',
+    },
+  ],
+};
