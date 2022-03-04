5a796a828f315fc1c23233256be9eda34efeaf7a
Ashish Thomas <ashishthomas2202@gmail.com>
2022-03-04 12:47:10 -0500

    Header created with menu, action and user with style

---
+++ components/Header/Header.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import Link from 'next/link';
+import style from './Header.module.scss';
+
+export default function Header() {
+  const menuItems = [
+    { name: 'Home', link: '/' },
+    { name: 'Shop', link: '/' },
+    { name: 'About', link: '/' },
+    { name: 'Contact', link: '/' },
+  ];
+
+  const menu = menuItems.map((item, i) => {
+    return (
+      <li key={'menu' + (i + 1)}>
+        <Link href={item.link}>
+          <a>{item.name}</a>
+        </Link>
+      </li>
+    );
+  });
+
+  const actionItems = [
+    { name: 'Favorite', link: '/' },
+    { name: 'Shopping Bag', link: '/' },
+  ];
+
+  const actions = actionItems.map((item, i) => {
+    return (
+      <li key={'action' + (i + 1)}>
+        <Link href={item.link}>
+          <a>{item.name}</a>
+        </Link>
+      </li>
+    );
+  });
+
+  const userItems = [
+    { name: 'Sign Up', link: '/' },
+    { name: 'Sign In', link: '/' },
+  ];
+
+  const user = userItems.map((item, i) => {
+    return (
+      <li key={'user' + (i + 1)}>
+        <Link href={item.link}>
+          <a>{item.name}</a>
+        </Link>
+      </li>
+    );
+  });
+
+  return (
+    <div className={style.header}>
+      <div className={style.container}>
+        <div className={style.brand}>Brand</div>
+        <ul>{menu}</ul>
+        <ul>{actions}</ul>
+        <ul className={style.user}>{user}</ul>
+      </div>
+    </div>
+  );
+}
