92e827ef81aeef76421791d0b15fbf330da53f2f
Ashish Thomas <ashishthomas2202@gmail.com>
2022-03-04 13:19:16 -0500

    refactored the header code

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -3,6 +3,10 @@ import Link from 'next/link';
 import style from './Header.module.scss';
 
 export default function Header() {
+  /****************** Brand ******************/
+  const brand = <div className={style.brand}>Brand</div>;
+
+  /****************** Menu ******************/
   const menuItems = [
     { name: 'Home', link: '/' },
     { name: 'Shop', link: '/shop' },
@@ -10,53 +14,67 @@ export default function Header() {
     { name: 'Contact', link: '/contact' },
   ];
 
-  const menu = menuItems.map((item, i) => {
-    return (
-      <li key={'menu' + (i + 1)}>
-        <Link href={item.link}>
-          <a>{item.name}</a>
-        </Link>
-      </li>
-    );
-  });
+  const menu = (
+    <ul>
+      {menuItems.map((item, i) => {
+        return (
+          <li key={'menu' + (i + 1)}>
+            <Link href={item.link}>
+              <a>{item.name}</a>
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
 
+  /****************** Action ******************/
   const actionItems = [
     { name: 'Favorite', link: '/favorite' },
     { name: 'Shopping Bag', link: '/shoppingBag' },
   ];
 
-  const actions = actionItems.map((item, i) => {
-    return (
-      <li key={'action' + (i + 1)}>
-        <Link href={item.link}>
-          <a>{item.name}</a>
-        </Link>
-      </li>
-    );
-  });
+  const actions = (
+    <ul>
+      {actionItems.map((item, i) => {
+        return (
+          <li key={'action' + (i + 1)}>
+            <Link href={item.link}>
+              <a>{item.name}</a>
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
 
+  /****************** User ******************/
   const userItems = [
     { name: 'Sign Up', link: '/' },
     { name: 'Sign In', link: '/' },
   ];
 
-  const user = userItems.map((item, i) => {
-    return (
-      <li key={'user' + (i + 1)}>
-        <Link href={item.link}>
-          <a>{item.name}</a>
-        </Link>
-      </li>
-    );
-  });
+  const user = (
+    <ul className={style.user}>
+      {userItems.map((item, i) => {
+        return (
+          <li key={'user' + (i + 1)}>
+            <Link href={item.link}>
+              <a>{item.name}</a>
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
 
   return (
     <div className={style.header}>
       <div className={style.container}>
-        <div className={style.brand}>Brand</div>
-        <ul>{menu}</ul>
-        <ul>{actions}</ul>
-        <ul className={style.user}>{user}</ul>
+        {brand}
+        {menu}
+        {actions}
+        {user}
       </div>
     </div>
   );
