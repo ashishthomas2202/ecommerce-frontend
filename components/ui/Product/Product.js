aab11c1b5185dd1eb7030ed78e495ad3fda0ff97
Ashish Thomas <ashishthomas2202@gmail.com>
2022-03-04 19:02:21 -0500

    working on the grid

--- components/ui/Product/Product.js
+++ components/ui/Product/Product.js
@@ -2,5 +2,5 @@ import React from 'react';
 import style from './Product.module.scss';
 
 export default function Product({ product }) {
-  return <div>Product</div>;
+  return <div className={style.product}>Product</div>;
 }
