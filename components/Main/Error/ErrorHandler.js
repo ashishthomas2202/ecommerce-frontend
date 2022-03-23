5ae568b4fe8a1e85fa59efc0a722f25288830934
Ashish Thomas <ashishthomas2202@gmail.com>
2022-03-23 14:20:39 -0400

    created new auth system with next-auth and protected shipping page

--- components/Main/Error/ErrorHandler.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-// import { useEffect } from 'react/cjs/react.production.min';
-
-export default function ErrorHandler({ error }) {
-  // useEffect()
-
-  //   const [errorState, setErrorState] = useState(false);
-  return <div style={{ color: 'red' }}>{error.err ? error.message : ''}</div>;
-}
